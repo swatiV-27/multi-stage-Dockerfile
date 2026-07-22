0d997c379f99bc1619d6ed61791f241ccd37abc9
root <root@ip-172-31-46-212.ec2.internal>
2026-07-22 11:09:51 +0000

    Initial commit: DevOps multi-stage Docker project

---
+++ src/app.js
@@ -0,0 +1,54 @@
+function showDocker(){
+
+document.getElementById("content").innerHTML=`
+<h3>Docker</h3>
+<p>
+Docker introduced containerization which allows
+applications to run consistently across environments.
+</p>
+`;
+}
+
+function showKubernetes(){
+
+document.getElementById("content").innerHTML=`
+<h3>Kubernetes</h3>
+<p>
+Kubernetes automates deployment, scaling,
+and management of containerized applications.
+</p>
+`;
+}
+
+function showTerraform(){
+
+document.getElementById("content").innerHTML=`
+<h3>Terraform</h3>
+<p>
+Terraform enables Infrastructure as Code,
+making infrastructure repeatable and version controlled.
+</p>
+`;
+}
+
+function showJenkins(){
+
+document.getElementById("content").innerHTML=`
+<h3>Jenkins</h3>
+<p>
+Jenkins automates build, test and deployment
+pipelines through CI/CD.
+</p>
+`;
+}
+
+function showAWS(){
+
+document.getElementById("content").innerHTML=`
+<h3>AWS</h3>
+<p>
+AWS provides scalable cloud services used
+to deploy modern applications globally.
+</p>
+`;
+}
